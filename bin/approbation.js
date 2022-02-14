1e7b6b8ad4661906cf8662cd43074e83c59ebc8e
Edd <edd@vega.xyz>
2022-02-14 15:32:16 +0000

    Import old code, add basic wrapper

---
+++ bin/approbation.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+const argv = require('minimist')(process.argv.slice(2))
+
+switch (argv._[0]) {
+  case 'check-codes':
+    require('../src/check-codes')
+    break
+  case 'check-references':
+    require('../src/check-references')
+    break
+  case 'check-filenames':
+    require('../src/check-filenames')
+    break
+  default:
+    console.log('Please choose a command')
+    process.exit(1)
+}
